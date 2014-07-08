3ef8b0c48697c15327ae9d2f14fdc1ff88a66427
Dmitri Akatov <akatov@gmail.com>
2014-07-08 16:15:54 +0100

    create input bar which creates new widgets. closes #5

--- app/routes/index.js
+++ app/routes/index.js
@@ -4,9 +4,17 @@ export default Ember.Route.extend({
   model: function(){
     return this.store.findAll('widget');
   },
+  position: 110,
   actions: {
     delete: function(widget){
       widget.deleteRecord();
+    },
+    create: function(text){
+      this.store.createRecord('widget', {
+        contents: text,
+        position: { x: this.position, y: this.position }
+      })
+      this.position += 10;
     }
   }
 });
